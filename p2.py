a2d7dad7c52e9f02a03f380ac1c863ff83bbbacb
Andres-Guane <gerardo.camargo@guane.com.co>
2021-07-14 17:22:31 -0500

    primeros ejer

---
+++ p2.py
@@ -0,0 +1,23 @@
+
+#actividad#
+print ('Hola mundo')
+#actividad#
+saludo = 'hola mundo'
+print (saludo)
+#actividad#
+nombre = input ('ingrese nombre ')
+print ('Hola ' + nombre)
+#actividad#
+print (((2+3)/(2*5))**2)
+horas = float (input('ingrese el numero de horas trabajadas '))
+valor = float (input('ingrese el valor de cada hora de trabajo '))
+pagar = valor * horas
+print ('total a pagar ', pagar)
+#actividad#
+sumanumero = float (input('ingreso un numero entero que quiere sumar '))
+resultado = sumanumero * (sumanumero + 1) / 2
+print('este es el resultado', resultado)
+#actividad#
+peso = float(input('ingrese el peso '))
+estatura = float (input('ingrese la estatura '))
+
