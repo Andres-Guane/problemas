a96e00a39f2aa482b4ffbaf444bd914f5d2aeb52
Andres-Guane <gerardo.camargo@guane.com.co>
2021-07-29 12:13:41 -0500

    actividad con simon terminada

--- simon.py
+++ simon.py
@@ -4,5 +4,24 @@ def sumas (n):
     for i in range (n,0,-1):
         s= s+i
     return s
-resultado = sumas(n)
-print(resultado)
+resultado1 = sumas(n)
+print(resultado1)
+
+def factorial (n):
+    s=1
+    for i in range (n,0,-1):
+        s = s*i
+    return s
+resultado2 = factorial(n)
+print(resultado2)
+#si es par la suma de las funcion factorial y suma 
+#si es impar producto de la factorial y suma 
+def final (n):
+    if n %2 ==0:
+       resultado =  resultado1+resultado2
+    else: resultado = resultado1*resultado2
+    return resultado
+resultado_final=final(n)
+print(resultado_final)
+   
+
